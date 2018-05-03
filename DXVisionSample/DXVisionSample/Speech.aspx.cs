6437afd39f25c2133db9c0cacf01a2608861adf1
Don Wibier <don@anykey.nl>
2018-05-03 11:11:25 +0200

    Small fixes

--- DXVisionSample/DXVisionSample/Speech.aspx.cs
+++ DXVisionSample/DXVisionSample/Speech.aspx.cs
@@ -15,14 +15,14 @@ namespace DXVisionSample
 		{
 
 		}
-        const string appId = "4af50150-a836-47ba-ace7-25eff41713cc";
+        const string appId = "0b7de52b-8fe6-4571-bffb-2041b550c3ef";
         const string key = "59990945ef334679b426b6a365957443";
         protected string Analyze(string text)
         {
             var client = new HttpClient();
             string cmd = HttpUtility.UrlEncode(text);
             // Execute the REST API call.
-            var url = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/{appId}?subscription-key={key}&staging=true&verbose=true&timezoneOffset=0&q={cmd}";
+            var url = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/{appId}?subscription-key={key}&verbose=true&timezoneOffset=0&q={cmd}";
             var response = client.GetStringAsync(url);
             Task.WaitAll(response);
             // Get the JSON response.
